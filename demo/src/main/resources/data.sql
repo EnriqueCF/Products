62e7403020d3792824cde6698743ea79b8d5e3c9
Enrique <enrique.casas@nfq.es>
2023-02-06 07:21:04 +0100

    [UPD] Modified path used in CSVREADER - Classpath

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO PRODUCT (id,sequence) SELECT * FROM CSVREAD('C:\DEV\00 - repos\Otros\Products\demo\src\main\resources\product.csv');
-INSERT INTO STOCK (sizeId,quantity) SELECT * FROM CSVREAD('C:\DEV\00 - repos\Otros\Products\demo\src\main\resources\stock.csv');
-INSERT INTO SIZE (id, productId, backSoon, special) SELECT * FROM CSVREAD('C:\DEV\00 - repos\Otros\Products\demo\src\main\resources\size.csv');
+INSERT INTO PRODUCT (id,sequence) SELECT * FROM CSVREAD('classpath:product.csv');
+INSERT INTO STOCK (sizeId,quantity) SELECT * FROM CSVREAD('classpath:stock.csv');
+INSERT INTO SIZE (id, productId, backSoon, special) SELECT * FROM CSVREAD('classpath:size.csv');
